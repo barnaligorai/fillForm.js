d020c865a2966f1494fa047803556d5e04dfa6ec
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2022-06-06 15:31:15 +0530

    Corrected setEncoding function.

--- fillForm.js
+++ fillForm.js
@@ -1,4 +1,5 @@
 /* eslint-disable max-statements */
+
 const process = require('process');
 const fs = require('fs');
 
@@ -10,7 +11,7 @@ const writeToFile = (data) => {
 
 const parseContent = (content, fileData, callBack) => {
   if (Object.keys(fileData).length === 0) {
-    fileData['name'] = content;
+    fileData['Name'] = content;
     console.log('Please enter your DOB : [yyyy-mm-dd]');
     return;
   }
@@ -22,20 +23,16 @@ const parseContent = (content, fileData, callBack) => {
   }
 
   const hobbies = content.split(',');
-  fileData.hobbies = hobbies;
+  fileData.Hobbies = hobbies;
   callBack(fileData);
 };
 
 const readFromStdin = (callBack) => {
   const fileData = {};
-  process.stdin.setEncoding = 'utf8';
-
-  let input = '';
+  process.stdin.setEncoding('utf8');
 
   process.stdin.on('data', (chunk) => {
-    input += chunk;
-    const content = input.split('\n');
-    input = '';
+    const content = chunk.split('\n');
     parseContent(content[0], fileData, callBack);
   });
 };
